df8827d8f269718ce77b62f5a66c9515d7014e49
henry <confiscate@gmail.com>
2016-10-08 20:20:02 -0700

    Schedule page done

--- app/src/main/java/com/chargebuddy/chargebuddy/ScheduleActivity.java
+++ app/src/main/java/com/chargebuddy/chargebuddy/ScheduleActivity.java
@@ -1,8 +1,13 @@
 package com.chargebuddy.chargebuddy;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.app.AlertDialog;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by henrylau on 10/8/16.
@@ -19,4 +24,46 @@ public class ScheduleActivity extends Activity {
         // Do something in response to button
     }
 
+
+    public void clickTime(String timeStr) {
+//        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(ScheduleActivity.this);
+        builder1.setMessage("Confirm reservation at\n\nMission Charging\n\nat\n\n" + timeStr);
+        builder1.setCancelable(true);
+
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+        TextView messageText = (TextView)alert11.findViewById(android.R.id.message);
+        messageText.setGravity(Gravity.CENTER);
+    }
+
+
+    public void clickTen(View view) {
+        clickTime("10:00 AM");
+    }
+
+    public void clickThree(View view) {
+        clickTime("3:00 PM");
+    }
+
+    public void clickFour(View view) {
+        clickTime("4:15 PM");
+    }
 }
